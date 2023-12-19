0a1e0f6d4fa247e1ace74b303f1413814c690094
kadir-zeren <k4dzeren@gmail.com>
2023-12-19 14:17:56 +0300

    error

---
+++ js-1/app.js
@@ -0,0 +1,11 @@
+console.log("Localde bulunan script calisti");
+
+alert("bu bir alertdir");
+
+karar = confirm("eminmisiniz");
+
+console.log(karar);
+
+console.warn("bu bir uyarıdır");
+
+console.error("eyvah hata oluştu");
