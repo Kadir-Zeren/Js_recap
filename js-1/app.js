0a3dec86d32e87c0f379f1f9a07d5f2c40466aa8
kadir-zeren <k4dzeren@gmail.com>
2023-12-19 16:09:25 +0300

    let const

--- js-1/app.js
+++ js-1/app.js
@@ -1,11 +1,26 @@
-console.log("Localde bulunan script calisti");
+// console.log("Localde bulunan script calisti");
 
-alert("bu bir alertdir");
+// alert("bu bir alertdir");
 
-karar = confirm("eminmisiniz");
+// karar = confirm("eminmisiniz");
 
-console.log(karar);
+// console.log(karar);
 
-console.warn("bu bir uyarıdır");
+// console.warn("bu bir uyarıdır");
 
-console.error("eyvah hata oluştu");
+// console.error("eyvah hata oluştu");
+
+console.log("APP js Running");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+let adet;
+console.log(adet);
+
+let language = " java";
+console.log(language, typeof language);
