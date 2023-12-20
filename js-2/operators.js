d4acd9c21bef4fc6463c41760e92f127e255c4c5
kadir-zeren <k4dzeren@gmail.com>
2023-12-20 10:17:04 +0300

    template literal

--- js-2/operators.js
+++ js-2/operators.js
@@ -1,1 +1,29 @@
 console.log("Js dosyası bağlandı");
+
+var deneme = 5;
+varDeneme = 6;
+
+let myVar = 5;
+const myVar1 = 6;
+
+const num1 = 10;
+const num2 = 69;
+
+const toplam = num1 + num2;
+console.log(toplam);
+
+const msg1 = "Hello";
+const msg2 = "World!";
+
+const mesaj = msg1 + msg2;
+console.log(mesaj);
+
+const firstName = "Koray";
+const lastName = "Koksal";
+
+console.log(firstName, "", lastName);
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName2);
